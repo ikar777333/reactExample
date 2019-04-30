016f629beddac6c315e910dcac5391e72e784ef6
ikarak <ikar777333@gmail.com>
2019-04-30 13:22:04 +0300

    Code without context. Again

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,15 +2,41 @@ import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./../Data"
 
-function MainContent() {
+class MainContent extends React.Component {
 
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
 
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
+    handleChange(id) {
+        console.log("Changed!")
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
+
 }
 
 export default MainContent
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,22 @@
 import React from "react"
 
-function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
-            <p>{props.item.text}</p>
-        </div>
-    )
+class TodoItem extends React.Component {
+
+    completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
+    render() {
+        return (
+            <div className="todo-item">
+                <input type="checkbox" onChange={() => this.props.handleChange(this.props.item.id)} checked={this.props.item.completed}/>
+                <p style={this.props.item.completed ? this.completedStyle: null}>{this.props.item.text}</p>
+            </div>
+        )
+    }
+    
 }
 
 export default TodoItem
